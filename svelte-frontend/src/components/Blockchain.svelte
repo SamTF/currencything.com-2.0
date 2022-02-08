a304e7ebdf0f5c4eab77b44ac4551f8d377d5d86
SamTF <elseban88@gmail.com>
2022-02-08 00:11:14 +0000

    Blockchain columns can all be sorted asc/desc !

--- svelte-frontend/src/components/Blockchain.svelte
+++ svelte-frontend/src/components/Blockchain.svelte
@@ -9,8 +9,10 @@
     export let table_data   // the blockchain data that gets displayed as a table
     export let milestones   // dictionary of currency thing milestones and their TX IDs
 
-    let columns = Object.keys(table_data[0]) // dynamically getting the keys of the data and using that as column headers
+    let columns = Object.keys(table_data[0])    // dynamically getting the keys of the data and using that as column headers
     // const columns = ['ID', 'INPUT', 'SIZE', 'OUTPUT', 'EMOTE', 'DATE']
+
+    let ascending = true                        // default sorting is by ascending ID (for now anyway)
 
     
     // Checks if the current TX is a milestone. If so, returns its milestone. Otherwise returns null.
@@ -21,15 +23,42 @@
             return null
         }
     }
+
+    // Sorting the array of objects by a property
+    const sortColumn = key => {           // -> https://stackoverflow.com/questions/7889006/sorting-arrays-in-javascript-by-object-key-value
+        ascending = !ascending
+
+        // checking if property is a string or a number (manual check for Emote field because they're all string except the first one, which is a 0)
+        if (isNaN(table_data[0][key]) || key === 'EMOTE') {
+            table_data = table_data.sort((a, b) => sortString(a[key], b[key]))
+        } else {
+            table_data = table_data.sort((a, b) => sortInt(a[key], b[key]))
+        }
+    }
+
+    // Sorting array by integer value
+    const sortInt = (a, b) => {
+        if (ascending)  return a - b
+        else            return a + b
+    }
+    // Sorting array by string value
+    const sortString = (a, b) => {
+        if (a < b)      return ascending ? -1 : 1   // if A comes before B: A is first if ascending
+        if (a > b)      return ascending ? 1 : -1   // if A comes after B: A is first if descending
+
+        return 0                                    // no change if they are the same
+    }
 </script>
 
 <!-- HTML -->
 <div class="table-container">
 <table class="blockchain-table">
     <!-- The Table Column Headers -->
-    <thead>
+    <thead class="clickable">
         {#each columns as column}
-            <th>{column}</th>
+            <th on:click={() => sortColumn(column)}>
+                {column}
+            </th>
         {/each}
     </thead>
 
