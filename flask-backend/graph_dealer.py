9454e6a350baa75a1685148186e40a7615129373
SamTF <elseban88@gmail.com>
2022-02-10 22:47:49 +0000

    Create Line or Bar charts from one function

--- flask-backend/graph_dealer.py
+++ flask-backend/graph_dealer.py
@@ -1,10 +1,15 @@
 # IMPORTS
 import matplotlib.pyplot as plt
 import pandas as pd
+from enum import Enum
 
 
 ### CONSTANTS
 GRAPH_DIR = '../svelte-frontend/static/graphs/{}.svg'
+
+class GraphType(Enum):
+    LINE = 1,
+    BAR = 2
 
 
 ###### HELPER FUNCTIONS ##############################################################################################################
@@ -20,12 +25,13 @@ def format_dates(array):
 
 
 ###### ACTUAL GRAPHING ##############################################################################################################
-def plot_line_chart(data: pd.DataFrame, file_name: str, title: str, xlabel: str = '# Of Trades', ylabel: str = '₡urrency Things', date_index:bool = False) -> str:
+def plot_chart(data: pd.DataFrame, graph_type: GraphType, file_name: str, title: str, xlabel: str = '# Of Trades', ylabel: str = '₡urrency Things', date_index:bool = False) -> str:
     '''
-    Generic plotting function for a line chart. Plots a DataFrame with the index as the X values and the SIZE column as the Y values.
+    Generic plotting function for a line or bar chart. Plots a DataFrame with the index as the X values and the SIZE column as the Y values.
     Save the graph as an SVG and returns the its file name.
 
     data: Pandas DataFrame with the values to plot. Must contain a SIZE column.
+    graph_type: LINE or BAR
     file_name: Name to save the SVG as.
     title: The title of the graph.
     xlabel: Label for the X-axis.
@@ -35,15 +41,25 @@ def plot_line_chart(data: pd.DataFrame, file_name: str, title: str, xlabel: str 
     # setting the graph size
     plt.figure(figsize=(8,6), dpi=150)
 
-    # formats dates nicely if the axis values are indeed dates
-    xaxis = data.index if not date_index else format_dates(data.index)
+    # line chart: formats dates nicely if the axis values are indeed dates
+    if graph_type == GraphType.LINE:
+        xaxis = data.index if not date_index else format_dates(data.index)
+    # bar chart: converting x-values from int to string removes empty space between values
+    elif graph_type == GraphType.BAR:
+        xaxis = data.index.astype(str) if not date_index else format_dates(data.index)
 
     # getting appropriate Y-data
     yaxis = data.SIZE if 'SIZE' in data else data.SUPPLY
 
-    # plotting the values 
-    plt.plot(xaxis, yaxis, c='xkcd:hot pink', linewidth = 0)
-    plt.fill_between(xaxis, yaxis, facecolor = 'xkcd:hot pink')     # filling the area under the line to make it look nice - > bloomberg.py
+
+    # plotting the values
+    if graph_type == GraphType.LINE:
+        plt.plot(xaxis, yaxis, c='xkcd:hot pink', linewidth = 0)
+        plt.fill_between(xaxis, yaxis, facecolor = 'xkcd:hot pink')     # filling the area under the line to make it look nice
+    
+    elif graph_type == GraphType.BAR:
+        plt.bar(xaxis, data['SIZE'], color = 'xkcd:hot pink', zorder=3) # needs a zorder to be rendered on top of the gridlines
+    
 
     # Titles
     plt.title(title,    fontdict={'fontname': 'Comic Sans MS', 'fontsize': 20, 'color': 'white'})
@@ -58,7 +74,12 @@ def plot_line_chart(data: pd.DataFrame, file_name: str, title: str, xlabel: str 
     ax.tick_params(colors="white", labelsize=10) # colour and size of the tick markers
 
     # Amount of ticks
-    ax.xaxis.set_major_locator(plt.MaxNLocator(6))  # sets a maximum amount of 6 ticks on the X Axis
+    if date_index:
+        ax.xaxis.set_major_locator(plt.MaxNLocator(6))  # sets a maximum amount of 6 ticks on the X Axis
+    
+    # Adding gridlines - with z-order of 0 to be behind the bars
+    if graph_type == GraphType.BAR:
+        plt.grid(axis='y', color = 'gray', linestyle = '--', linewidth = 0.5, zorder=0)
 
     # Saving the graph as an SVG
     plt.savefig(GRAPH_DIR.format(file_name), transparent=True)
@@ -70,11 +91,6 @@ def plot_line_chart(data: pd.DataFrame, file_name: str, title: str, xlabel: str 
 
 
 if __name__ == '__main__':
-    from explorer import Explorer
-    ex = Explorer()
-    data = ex.supply_over_time()
-
-    print(data)
-
-    g = plot_line_chart(data, 'supply', 'Supply Over Time', 'Time', date_index=True)
-    print(g)
+    print('executed when invoked directly')
+else:
+    print('[GRAPH_DEALER.PY IMPORTED]')
