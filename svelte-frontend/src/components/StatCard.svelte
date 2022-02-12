014a069dffd3d8b6dea8928609aead28dc1a7203
SamTF <elseban88@gmail.com>
2022-02-12 15:55:43 +0000

    Allow for cards with just a title

--- svelte-frontend/src/components/StatCard.svelte
+++ svelte-frontend/src/components/StatCard.svelte
@@ -11,7 +11,7 @@
     import { slide } from 'svelte/transition'
 
     // Props
-    export let data = 3             // the data to display in the text field
+    export let data                 // the data to display in the text field
     export let label = 'title'      // text to display after the data
     export let colour = 'accent'    // background colour of the card
     export let phrase = ''          // phrase to display at the end, if any (ex: in the last 24h, this week, total)
@@ -34,7 +34,13 @@
 <div class="stat-panel clickable" on:click={toggle}>
     <!-- Statistics Info Box -->
     <div class={`stat-card bg-${colour}`}>
-        <h2>...<b>{data}</b> {label} {phrase}</h2>
+        <!-- Only display data if there is data to display -->
+        {#if data}
+            <h2>...<b>{data}</b> {label} {phrase}</h2>
+        <!-- otherwise just display the label -->
+        {:else}
+            <h2>{label}</h2>
+        {/if}
     </div>
 
     <!-- Expanded Graph -->
