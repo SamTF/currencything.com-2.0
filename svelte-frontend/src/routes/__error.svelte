36d4db05032b79f562126e5ada0bc4820ca23bb2
SamTF <elseban88@gmail.com>
2022-02-14 23:59:48 +0000

    Created my own error page

---
+++ svelte-frontend/src/routes/__error.svelte
@@ -0,0 +1,26 @@
+<!-- SSR -->
+<script context="module">
+    export async function load({error, status}) {
+        return {props: {error, status}}
+    }
+</script>
+<!-- JS -->
+<script>
+    export let error
+    export let status
+</script>
+
+<!-- HTML -->
+<div class="err">
+    <h1>:(</h1>
+    <h2>{error.message}</h2>
+
+    {#if import.meta.env.DEV}
+        <h2>{error.name} {status}</h2>
+        <code>{JSON.stringify(error)}</code>
+    {/if}
+
+    {#if import.meta.env.PROD}
+        <h1>YEEEEEEEE</h1>
+    {/if}
+</div>
