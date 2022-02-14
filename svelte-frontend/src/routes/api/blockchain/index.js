c9d1bc6e45fc3503c45bc606e2e4fd1bceba53b5
SamTF <elseban88@gmail.com>
2022-02-14 18:57:42 +0000

    Added option to sort /blockchain descendingly

--- svelte-frontend/src/routes/api/blockchain/index.js
+++ svelte-frontend/src/routes/api/blockchain/index.js
@@ -1,11 +1,13 @@
 // # Basically a proxy for the backend Flask API
 export async function get({params, url}) {
-    const descending = url.searchParams.get('descending')
-    console.log(descending)
-
-
+    // Gettinh Flask API URL from the .env file
     const BASE_URL = import.meta.env.VITE_API_URL
-    const URL = `${BASE_URL}/blockchain?descending=True`
+    let URL = `${BASE_URL}/blockchain`
+
+    // adding descending option to the URL request if that parameter is present
+    const descending = url.searchParams.get('descending')
+    if (descending) URL += '?descending=True'
+
     const res = await fetch(URL)
     const data = await res.json()
 
